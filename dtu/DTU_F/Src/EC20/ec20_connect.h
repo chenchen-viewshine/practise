8988fec4854e9b4fe072a7fd144200588c270e53
chenchen-viewshine <chenchen@viewshine.cn>
2018-07-05 18:30:02 +0800

    【删除】主要文件

--- dtu/DTU_F/Src/EC20/ec20_connect.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EC20_CONNECT_H__
-#define __EC20_CONNECT_H__
-
-
-void EC20_connect(void);
-
-
-#endif // #ifndef __EC20_CONNECT_H__
--- dtu/DTU_F/Src/EC20/ec20_transmit.c
+++
@@ -1,6 +0,0 @@
-#include "ec20_transmit.h"
-
-// 数据收发
-void EC20_transmit(void)
-{
-}
